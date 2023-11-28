28c2bc01fab2d1d230e6034432ab8c8c2b970abb
IrsyadFikriansyah <irsyadfikriansyah@gmail.com>
2023-11-29 00:13:31 +0700

    initial commit

---
+++ app/Config/Routes.php
@@ -0,0 +1,19 @@
+<?php
+
+use CodeIgniter\Router\RouteCollection;
+
+/**
+ * @var RouteCollection $routes
+ */
+$routes->get('/', 'Home::index');
+
+$routes->get('/post', 'Post::index');
+
+$routes->get('/post/create', 'Post::create');
+$routes->post('/post/store', 'Post::store');
+
+$routes->get('/post/edit/(:any)', 'Post::edit/$1');
+$routes->post('/post/update/(:any)', 'Post::update/$1');
+
+$routes->get('/post/delete/(:any)', 'Post::delete/$1');
+
